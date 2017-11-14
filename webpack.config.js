4fa86bdc9746f5a76cf1a42a62450517e49cfef7
Michael Leaney <mleaney@tokbox.com>
2017-11-14 11:29:28 +0800

    webpack changes to extract css/scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 var path = require('path')
 var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -8,8 +10,8 @@ module.exports = {
     'vue-recyclist.min': './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, './example'),
-    publicPath: process.env.NODE_ENV === 'production' ? './example/' : 'example/',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: process.env.NODE_ENV === 'production' ? './' : 'example/',
     filename: '[name].js',
     library: 'VueRecyclist',
     libraryTarget: 'umd'
@@ -17,15 +19,29 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
+          extractCSS: true,
           loaders: {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            scss: ExtractTextPlugin.extract({
+              fallback: 'vue-style-loader',
+              use: ['css-loader', 'sass-loader']
+            }),
+            sass: ExtractTextPlugin.extract({
+              fallback: 'vue-style-loader',
+              use: ['css-loader', 'sass-loader?indentedSyntax']
+            })
           }
           // other vue-loader options go here
         }
@@ -45,20 +61,27 @@ module.exports = {
       }
     ]
   },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.' + (process.env.NODE_ENV === 'production' ? 'min' : 'common') + '.js'
-    }
-  },
   devtool: 'source-map',
   devServer: {
     historyApiFallback: true,
     noInfo: true,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    contentBase: [path.join(__dirname, 'example'), path.join(__dirname, 'dist')],
   },
   performance: {
     hints: false
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: (getPath) => {
+        console.log('path', getPath('[name].css'));
+        return getPath('[name].css');
+      },
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+    }),
+  ],
 }
 
 if (process.env.NODE_ENV === 'production') {
